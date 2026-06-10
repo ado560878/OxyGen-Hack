edc3b5bb4522179597de2d13e6e7d08e30db83c8
AdoLife-creator <order560878@gmail.com>
2026-06-10 17:19:09 +0300

    Update GameRendererMixin.java

--- src/main/java/com/oxygenclient/module/mixin/GameRendererMixin.java
+++ src/main/java/com/oxygenclient/module/mixin/GameRendererMixin.java
@@ -2,15 +2,11 @@ package com.oxygenclient.mixin;
 
 import com.oxygenclient.OxygenClient;
 import com.oxygenclient.module.Module;
-import com.oxygenclient.module.render.XRayBypass;
+import com.oxygenclient.module.render.XRay;
 import net.minecraft.block.BlockState;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
-import org.joml.Matrix4f;
+import net.minecraft.world.BlockRenderView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,23 +14,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class GameRendererMixin {
-
     @Inject(method = "renderBlock", at = @At("HEAD"), cancellable = true)
-    private void onRenderBlock(BlockState state, BlockPos pos, 
-                                net.minecraft.world.BlockRenderView world,
-                                MatrixStack matrices,
-                                VertexConsumerProvider vertexConsumers,
+    private void onRenderBlock(BlockState state, BlockPos pos, BlockRenderView world,
+                                net.minecraft.client.util.math.MatrixStack matrices,
+                                net.minecraft.client.render.VertexConsumerProvider vcp,
                                 boolean cull,
                                 net.minecraft.util.math.random.Random random,
-                                LightmapTextureManager lightmap,
-                                int overlay,
-                                CallbackInfo ci) {
-        
-        Module xray = OxygenClient.moduleManager.getModule("XRay");
-        if (xray != null && xray.isEnabled()) {
-            if (!XRayBypass.shouldRender(state.getBlock())) {
-                ci.cancel();
-            }
+                                net.minecraft.client.render.LightmapTextureManager lightmap,
+                                int overlay, CallbackInfo ci) {
+        Module xray = OxygenClient.moduleManager.getModules().stream()
+            .filter(m -> m.getName().equals("XRay")).findFirst().orElse(null);
+        if (xray != null && xray.isEnabled() && !XRay.BLOCKS.contains(state.getBlock())) {
+            ci.cancel();
         }
     }
 }
