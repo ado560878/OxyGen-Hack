68c2fc62bd85ad8aacceaf0554d451e3253fffc4
AdoLife-creator <order560878@gmail.com>
2026-06-10 17:51:46 +0300

    Update Step.java

--- src/main/java/com/oxygenclient/module/movement/Step.java
+++ src/main/java/com/oxygenclient/module/movement/Step.java
@@ -5,16 +5,6 @@ import com.oxygenclient.module.Module;
 
 public class Step extends Module {
     public Step() {
-        super("Step", "Auto step blocks", Category.MOVEMENT);
-    }
-
-    @Override
-    public void onTick() {
-        if (mc.player != null) mc.player.setStepHeight(2.5f);
-    }
-
-    @Override
-    public void onDisable() {
-        if (mc.player != null) mc.player.setStepHeight(0.6f);
+        super("Step", Category.MOVEMENT);
     }
 }
