058ddabf9ae405c08181855dcb14330db961c46b
AdoLife-creator <order560878@gmail.com>
2026-06-10 17:13:25 +0300

    Create Velocity.java

---
+++ src/main/java/com/oxygenclient/module/combat/Velocity.java
@@ -0,0 +1,10 @@
+package com.oxygenclient.module.combat;
+
+import com.oxygenclient.module.Category;
+import com.oxygenclient.module.Module;
+
+public class Velocity extends Module {
+    public Velocity() {
+        super("Velocity", "No knockback", Category.COMBAT);
+    }
+}
