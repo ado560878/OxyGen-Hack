09d2ef55009e5422f75403c7a59c85c8a5074b95
AdoLife-creator <order560878@gmail.com>
2026-06-10 17:48:00 +0300

    Update Reach.java

--- src/main/java/com/oxygenclient/module/combat/Reach.java
+++ src/main/java/com/oxygenclient/module/combat/Reach.java
@@ -4,7 +4,5 @@ import com.oxygenclient.module.Category;
 import com.oxygenclient.module.Module;
 
 public class Reach extends Module {
-    public Reach() {
-        super("Reach", "Extended attack range", Category.COMBAT);
-    }
+    public Reach() { super("Reach", Category.COMBAT); }
 }
