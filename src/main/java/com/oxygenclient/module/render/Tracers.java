128073d228fe796664c7f92fbc51eef8f06c8f83
AdoLife-creator <order560878@gmail.com>
2026-06-10 17:53:07 +0300

    Update Tracers.java

--- src/main/java/com/oxygenclient/module/render/Tracers.java
+++ src/main/java/com/oxygenclient/module/render/Tracers.java
@@ -5,6 +5,6 @@ import com.oxygenclient.module.Module;
 
 public class Tracers extends Module {
     public Tracers() {
-        super("Tracers", "Draw lines to players", Category.RENDER);
+        super("Tracers", Category.RENDER);
     }
 }
