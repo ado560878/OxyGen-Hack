6e7a8e36c0ad1b79bcaf45bf9f925be300ecda9b
AdoLife-creator <order560878@gmail.com>
2026-06-10 17:13:37 +0300

    Update HitBox.java

--- src/main/java/com/oxygenclient/module/combat/HitBox.java
+++ src/main/java/com/oxygenclient/module/combat/HitBox.java
@@ -1,20 +1,10 @@
 package com.oxygenclient.module.combat;
 
-import com.oxygenclient.bypass.HitboxCorrector;
 import com.oxygenclient.module.Category;
 import com.oxygenclient.module.Module;
-import net.minecraft.entity.Entity;
-import net.minecraft.util.math.Box;
 
 public class HitBox extends Module {
-    private double expansion = 0.15;
-
     public HitBox() {
-        super("HitBox", "Vuruş alanını genişlet", Category.COMBAT);
-    }
-
-    public Box getModifiedHitbox(Entity entity, Box original) {
-        if (!isEnabled()) return original;
-        return HitboxCorrector.getExpandedHitbox(entity, expansion);
+        super("HitBox", "Expanded hitboxes", Category.COMBAT);
     }
 }
