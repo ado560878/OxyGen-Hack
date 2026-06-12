a331d92b779edb42b1b02c44aa615bced35040c6
ado560878 <ado560878@gmail.com>
2026-06-13 01:44:22 +0300

    Create Disabler.java

---
+++ src/main/java/com/oxygenclient/module/world/Disabler.java
@@ -0,0 +1,10 @@
+package com.oxygenclient.module.world;
+
+import com.oxygenclient.module.Category;
+import com.oxygenclient.module.Module;
+
+public class Disabler extends Module {
+    public Disabler() {
+        super("Disabler", "Disable anticheat", Category.WORLD);
+    }
+}
