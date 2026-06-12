41858a4b537b251de46fe0fb1ba2671ed973e894
ado560878 <ado560878@gmail.com>
2026-06-13 01:53:18 +0300

    Update Tracers.java

--- src/main/java/com/oxygenclient/module/render/Tracers.java
+++ src/main/java/com/oxygenclient/module/render/Tracers.java
@@ -5,6 +5,6 @@ import com.oxygenclient.module.Module;
 
 public class Tracers extends Module {
     public Tracers() {
-        super("Tracers", Category.RENDER);
+        super("Tracers", "Draw lines to players", Category.RENDER);
     }
 }
