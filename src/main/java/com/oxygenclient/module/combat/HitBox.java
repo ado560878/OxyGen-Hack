955640889f3f6ef272bc24c067d8df9d114acccb
ado560878 <ado560878@gmail.com>
2026-06-13 01:57:53 +0300

    Create HitBox.java

---
+++ src/main/java/com/oxygenclient/module/combat/HitBox.java
@@ -0,0 +1,10 @@
+package com.oxygenclient.module.combat;
+
+import com.oxygenclient.module.Category;
+import com.oxygenclient.module.Module;
+
+public class HitBox extends Module {
+    public HitBox() {
+        super("HitBox", "Expanded hitboxes", Category.COMBAT);
+    }
+}
