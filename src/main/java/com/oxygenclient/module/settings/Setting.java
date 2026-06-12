5c778e8fd89773272c9ea1be6270d19ff4191b74
ado560878 <ado560878@gmail.com>
2026-06-13 01:37:30 +0300

    Create Setting.java

---
+++ src/main/java/com/oxygenclient/module/settings/Setting.java
@@ -0,0 +1,14 @@
+package com.oxygenclient.module.settings;
+
+public abstract class Setting {
+    private final String name;
+    private final String description;
+
+    public Setting(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+}
