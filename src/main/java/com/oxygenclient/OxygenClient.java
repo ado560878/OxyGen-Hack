7db630dfdd00d03920c333cfd13ff01c1133c7e3
ado560878 <ado560878@gmail.com>
2026-06-13 02:50:30 +0300

    Update OxygenClient.java

--- src/main/java/com/oxygenclient/OxygenClient.java
+++ src/main/java/com/oxygenclient/OxygenClient.java
@@ -5,12 +5,13 @@ import net.fabricmc.api.ClientModInitializer;
 
 public class OxygenClient implements ClientModInitializer {
     public static final String NAME = "OxyGen";
-    public static final String VERSION = "4.0.0";
+    public static final String VERSION = "4.0";
     public static ModuleManager moduleManager;
 
     @Override
     public void onInitializeClient() {
         moduleManager = new ModuleManager();
-        System.out.println("[" + NAME + "] v" + VERSION + " | " + moduleManager.getModules().size() + " modules");
+        System.out.println("[" + NAME + "] v" + VERSION + " | " + 
+            moduleManager.getModules().size() + " modules loaded");
     }
 }
