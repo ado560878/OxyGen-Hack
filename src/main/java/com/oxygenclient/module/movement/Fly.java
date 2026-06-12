24cafed3ae20be66a5f87fbae8ddc4109c563824
ado560878 <ado560878@gmail.com>
2026-06-13 01:40:55 +0300

    Update Fly.java

--- src/main/java/com/oxygenclient/module/movement/Fly.java
+++ src/main/java/com/oxygenclient/module/movement/Fly.java
@@ -2,10 +2,14 @@ package com.oxygenclient.module.movement;
 
 import com.oxygenclient.module.Category;
 import com.oxygenclient.module.Module;
+import com.oxygenclient.module.settings.NumberSetting;
 
 public class Fly extends Module {
+    private final NumberSetting flySpeed = new NumberSetting("Speed", "Fly speed", 0.4, 0.1, 2.0, 0.1);
+
     public Fly() {
-        super("Fly", Category.MOVEMENT);
+        super("Fly", "Enable flying", Category.MOVEMENT);
+        addSetting(flySpeed);
     }
 
     @Override
@@ -13,6 +17,7 @@ public class Fly extends Module {
         if (mc.player != null) {
             mc.player.getAbilities().flying = true;
             mc.player.getAbilities().allowFlying = true;
+            mc.player.getAbilities().setFlySpeed((float)flySpeed.getValue() / 10);
         }
     }
 
@@ -21,6 +26,7 @@ public class Fly extends Module {
         if (mc.player != null && !mc.player.isCreative()) {
             mc.player.getAbilities().flying = false;
             mc.player.getAbilities().allowFlying = false;
+            mc.player.getAbilities().setFlySpeed(0.05f);
         }
     }
 }
