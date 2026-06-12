da94b19309546e1476d5e6429b5e224318497e07
ado560878 <ado560878@gmail.com>
2026-06-13 01:40:01 +0300

    Update Reach.java

--- src/main/java/com/oxygenclient/module/combat/Reach.java
+++ src/main/java/com/oxygenclient/module/combat/Reach.java
@@ -2,9 +2,13 @@ package com.oxygenclient.module.combat;
 
 import com.oxygenclient.module.Category;
 import com.oxygenclient.module.Module;
+import com.oxygenclient.module.settings.NumberSetting;
 
 public class Reach extends Module {
+    private final NumberSetting distance = new NumberSetting("Distance", "Reach distance", 3.5, 3.0, 6.0, 0.1);
+
     public Reach() {
-        super("Reach", Category.COMBAT);
+        super("Reach", "Extended reach", Category.COMBAT);
+        addSetting(distance);
     }
 }
