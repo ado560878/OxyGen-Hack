22d99056776911c76824e5b276994fb9d2912114
ado560878 <ado560878@gmail.com>
2026-06-14 01:43:31 +0300

    Update Module.java

--- src/main/java/com/oxygenclient/module/Module.java
+++ src/main/java/com/oxygenclient/module/Module.java
@@ -2,44 +2,53 @@ package com.oxygenclient.module;
 
 import com.oxygenclient.module.settings.Setting;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Module {
-    protected final MinecraftClient mc = MinecraftClient.getInstance();
-    public String name, description;
-    public Category category;
-    public boolean enabled;
-    public int key;
-    public List<Setting> settings = new ArrayList<>();
-
+public class Module {
+    protected String name;
+    protected String description;
+    protected Category category;
+    protected boolean enabled;
+    protected int key;
+    protected List<Setting> settings = new ArrayList<>();
+    
+    protected static MinecraftClient mc = MinecraftClient.getInstance();
+    
     public Module(String name, String description, Category category) {
         this.name = name;
         this.description = description;
         this.category = category;
-        this.enabled = false;
-        this.key = 0;
     }
-
-    public void toggle() {
-        enabled = !enabled;
-        if (enabled) onEnable(); else onDisable();
-        if (mc.player != null) {
-            mc.player.sendMessage(Text.literal((enabled ? "§a✔ " : "§c✘ ") + name + (enabled ? " ON" : " OFF")), true);
-        }
-    }
-
-    public void addSetting(Setting s) { settings.add(s); }
-    public void addSettings(Setting... s) { for (Setting ss : s) addSetting(ss); }
-    public void onEnable() {}
-    public void onDisable() {}
-    public void onTick() {}
-
+    
     public String getName() { return name; }
+    public String getDescription() { return description; }
     public Category getCategory() { return category; }
     public boolean isEnabled() { return enabled; }
     public int getKey() { return key; }
-    public void setKey(int k) { this.key = k; }
+    public void setKey(int key) { this.key = key; }
     public List<Setting> getSettings() { return settings; }
+    
+    public void toggle() {
+        enabled = !enabled;
+        if (enabled) onEnable();
+        else onDisable();
+    }
+    
+    public void onEnable() {}
+    public void onDisable() {}
+    public void onTick() {}
+    
+    public String getDisplayValue() {
+        // KillAura için güç değeri göster
+        if (this.name.equals("KillAura")) {
+            for (Setting s : settings) {
+                if (s.getName().equals("Range")) {
+                    return ((com.oxygenclient.module.settings.NumberSetting)s).getValue() + "";
+                }
+            }
+        }
+        return null;
+    }
 }
