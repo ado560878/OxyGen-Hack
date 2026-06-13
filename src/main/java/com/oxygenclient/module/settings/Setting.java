e458348d0e3316251a010ff5d6e5f5a54cae9a60
ado560878 <ado560878@gmail.com>
2026-06-14 01:50:22 +0300

    Update Setting.java

--- src/main/java/com/oxygenclient/module/settings/Setting.java
+++ src/main/java/com/oxygenclient/module/settings/Setting.java
@@ -1,14 +1,14 @@
 package com.oxygenclient.module.settings;
 
 public class Setting {
-    public String name;
-    public String description;
-    public boolean visible = true;
-
+    private String name;
+    private String description;
+    
     public Setting(String name, String description) {
         this.name = name;
         this.description = description;
     }
-
+    
     public String getName() { return name; }
+    public String getDescription() { return description; }
 }
