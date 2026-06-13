d017d46f6406c82897df4aaf79010d07a35e7aac
ado560878 <ado560878@gmail.com>
2026-06-14 01:50:35 +0300

    Update NumberSetting.java

--- src/main/java/com/oxygenclient/module/settings/NumberSetting.java
+++ src/main/java/com/oxygenclient/module/settings/NumberSetting.java
@@ -2,12 +2,12 @@ package com.oxygenclient.module.settings;
 
 public class NumberSetting extends Setting {
     private double value;
-    private final double min;
-    private final double max;
-    private final double increment;
+    private double min;
+    private double max;
+    private double increment;
     
-    public NumberSetting(String name, double defaultValue, double min, double max, double increment) {
-        super(name);
+    public NumberSetting(String name, String description, double defaultValue, double min, double max, double increment) {
+        super(name, description);
         this.value = defaultValue;
         this.min = min;
         this.max = max;
