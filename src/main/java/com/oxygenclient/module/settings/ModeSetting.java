f43f9359730e1dffd056fd4904a440ac0f2e0cbb
ado560878 <ado560878@gmail.com>
2026-06-14 01:51:01 +0300

    Update ModeSetting.java

--- src/main/java/com/oxygenclient/module/settings/ModeSetting.java
+++ src/main/java/com/oxygenclient/module/settings/ModeSetting.java
@@ -5,33 +5,21 @@ import java.util.List;
 
 public class ModeSetting extends Setting {
     private int index;
-    private final List<String> modes;
+    private List<String> modes;
     
-    public ModeSetting(String name, String defaultValue, String... modes) {
-        super(name);
+    public ModeSetting(String name, String description, String defaultValue, String... modes) {
+        super(name, description);
         this.modes = Arrays.asList(modes);
         this.index = this.modes.indexOf(defaultValue);
         if (this.index == -1) this.index = 0;
     }
     
-    public String getMode() { 
-        return modes.get(index); 
-    }
-    
+    public String getMode() { return modes.get(index); }
     public void setMode(String mode) { 
         int newIndex = modes.indexOf(mode);
         if (newIndex != -1) this.index = newIndex;
     }
-    
-    public void nextMode() { 
-        index = (index + 1) % modes.size(); 
-    }
-    
-    public void prevMode() { 
-        index = (index - 1 + modes.size()) % modes.size(); 
-    }
-    
-    public List<String> getModes() { 
-        return modes; 
-    }
+    public void nextMode() { index = (index + 1) % modes.size(); }
+    public void prevMode() { index = (index - 1 + modes.size()) % modes.size(); }
+    public List<String> getModes() { return modes; }
 }
