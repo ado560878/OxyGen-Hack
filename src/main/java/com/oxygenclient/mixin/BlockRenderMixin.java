f072a260ee9c01093c8e96820df2a4ef16392956
ado560878 <ado560878@gmail.com>
2026-06-14 02:23:26 +0300

    Update BlockRenderMixin.java

--- src/main/java/com/oxygenclient/mixin/BlockRenderMixin.java
+++ src/main/java/com/oxygenclient/mixin/BlockRenderMixin.java
@@ -15,44 +15,31 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Mixin(BlockRenderManager.class)
 public class BlockRenderMixin {
     
-    // Görünecek bloklar (cevherler)
-    private static final List<Block> VISIBLE_BLOCKS = Arrays.asList(
-        Blocks.IRON_ORE,
-        Blocks.DEEPSLATE_IRON_ORE,
-        Blocks.GOLD_ORE,
-        Blocks.DEEPSLATE_GOLD_ORE,
-        Blocks.DIAMOND_ORE,
-        Blocks.DEEPSLATE_DIAMOND_ORE,
-        Blocks.EMERALD_ORE,
-        Blocks.DEEPSLATE_EMERALD_ORE,
-        Blocks.LAPIS_ORE,
-        Blocks.DEEPSLATE_LAPIS_ORE,
-        Blocks.REDSTONE_ORE,
-        Blocks.DEEPSLATE_REDSTONE_ORE,
-        Blocks.COAL_ORE,
-        Blocks.DEEPSLATE_COAL_ORE,
-        Blocks.COPPER_ORE,
-        Blocks.DEEPSLATE_COPPER_ORE,
-        Blocks.NETHER_QUARTZ_ORE,
-        Blocks.NETHER_GOLD_ORE,
+    private static final Set<Block> ORES = new HashSet<>(Arrays.asList(
+        Blocks.IRON_ORE, Blocks.DEEPSLATE_IRON_ORE,
+        Blocks.GOLD_ORE, Blocks.DEEPSLATE_GOLD_ORE,
+        Blocks.DIAMOND_ORE, Blocks.DEEPSLATE_DIAMOND_ORE,
+        Blocks.EMERALD_ORE, Blocks.DEEPSLATE_EMERALD_ORE,
+        Blocks.LAPIS_ORE, Blocks.DEEPSLATE_LAPIS_ORE,
+        Blocks.REDSTONE_ORE, Blocks.DEEPSLATE_REDSTONE_ORE,
+        Blocks.COAL_ORE, Blocks.DEEPSLATE_COAL_ORE,
+        Blocks.COPPER_ORE, Blocks.DEEPSLATE_COPPER_ORE,
+        Blocks.NETHER_QUARTZ_ORE, Blocks.NETHER_GOLD_ORE,
         Blocks.ANCIENT_DEBRIS
-    );
+    ));
     
     @Inject(method = "renderBlock", at = @At("HEAD"), cancellable = true)
     private void onRenderBlock(BlockState state, BlockPos pos, BlockRenderView world, MatrixStack matrices,
                                VertexConsumerProvider vertexConsumers, boolean cull, CallbackInfo ci) {
         
-        // XRay modu açık mı?
         if (XRay.isXRayEnabled()) {
-            Block block = state.getBlock();
-            
-            // Eğer blok görünür bloklar listesinde değilse render etme
-            if (!VISIBLE_BLOCKS.contains(block)) {
+            if (!ORES.contains(state.getBlock())) {
                 ci.cancel();
             }
         }
