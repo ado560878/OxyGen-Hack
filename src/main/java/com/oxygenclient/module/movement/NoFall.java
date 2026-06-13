97bbdea26744b171ccc498d3789a5533dec616b4
ado560878 <ado560878@gmail.com>
2026-06-14 01:23:26 +0300

    Update NoFall.java

--- src/main/java/com/oxygenclient/module/movement/NoFall.java
+++ src/main/java/com/oxygenclient/module/movement/NoFall.java
@@ -2,15 +2,18 @@ package com.oxygenclient.module.movement;
 
 import com.oxygenclient.module.Category;
 import com.oxygenclient.module.Module;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class NoFall extends Module {
     public NoFall() {
-        super("NoFall", "No fall damage", Category.MOVEMENT);
+        super("NoFall", "Prevents fall damage", Category.MOVEMENT);
     }
 
     @Override
     public void onTick() {
-        if (mc.player != null && mc.player.fallDistance > 2.5f) {
+        if (mc.player == null || mc.getNetworkHandler() == null) return;
+        if (mc.player.fallDistance > 2f) {
+            mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
             mc.player.fallDistance = 0;
         }
     }
