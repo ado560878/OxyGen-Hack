d6c67c94a8738bec54b189576c8ec70d916f1e5e
ado560878 <ado560878@gmail.com>
2026-06-14 01:47:47 +0300

    Update ModuleManager.java

--- src/main/java/com/oxygenclient/module/ModuleManager.java
+++ src/main/java/com/oxygenclient/module/ModuleManager.java
@@ -4,34 +4,61 @@ import com.oxygenclient.module.combat.*;
 import com.oxygenclient.module.movement.*;
 import com.oxygenclient.module.render.*;
 import com.oxygenclient.module.world.*;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ModuleManager {
-    private final List<Module> modules = new ArrayList<>();
-
-    public ModuleManager() {
-        add(new KillAura());
-        add(new SilentAim());
-        add(new AutoClicker());
-        add(new Reach());
-        add(new Velocity());
-        add(new Criticals());
-        add(new Speed());
-        add(new Fly());
-        add(new NoFall());
-        add(new Sprint());
-        add(new Jesus());
-        add(new XRay());
-        add(new ESP());
-        add(new Fullbright());
-        add(new Disabler());
+    private static List<Module> modules = new ArrayList<>();
+    
+    public static void init() {
+        // Combat
+        modules.add(new KillAura());
+        modules.add(new AutoClicker());
+        modules.add(new SilentAim());
+        modules.add(new Reach());
+        modules.add(new Velocity());
+        modules.add(new Criticals());
+        
+        // Movement
+        modules.add(new Speed());
+        modules.add(new Fly());
+        modules.add(new NoFall());
+        modules.add(new Sprint());
+        modules.add(new Jesus());
+        
+        // Render
+        modules.add(new XRay());
+        modules.add(new ESP());
+        modules.add(new Fullbright());
+        
+        // World
+        modules.add(new Disabler());
     }
-
-    private void add(Module m) { modules.add(m); }
-    public List<Module> getModules() { return modules; }
-    public List<Module> getByCategory(Category c) {
-        return modules.stream().filter(m -> m.category == c).collect(Collectors.toList());
+    
+    public static List<Module> getModules() {
+        return modules;
     }
-    public void onTick() { modules.stream().filter(m -> m.enabled).forEach(Module::onTick); }
+    
+    public static List<Module> getModulesInCategory(Category category) {
+        return modules.stream()
+                .filter(m -> m.getCategory() == category)
+                .collect(Collectors.toList());
+    }
+    
+    public static Module getModuleByName(String name) {
+        return modules.stream()
+                .filter(m -> m.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+    
+    public static void onTick() {
+        for (Module module : modules) {
+            if (module.isEnabled()) {
+                module.onTick();
+            }
+        }
+    }
 }
