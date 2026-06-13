911b75208d530bf6baa41c6576f9662b46066632
ado560878 <ado560878@gmail.com>
2026-06-14 02:08:50 +0300

    Update OxygenClient.java

--- src/main/java/com/oxygenclient/OxygenClient.java
+++ src/main/java/com/oxygenclient/OxygenClient.java
@@ -1,16 +1,35 @@
 package com.oxygenclient;
 
 import com.oxygenclient.module.ModuleManager;
-import net.fabricmc.api.ClientModInitializer;
+import com.oxygenclient.ui.ClickGUI;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
-public class OxygenClient implements ClientModInitializer {
+public class OxygenClient implements ModInitializer {
     public static final String NAME = "OxyGen";
     public static final String VERSION = "5.0";
-    public static ModuleManager moduleManager;
-
+    
+    public static KeyBinding openClickGui;
+    
     @Override
-    public void onInitializeClient() {
-        moduleManager = new ModuleManager();
-        System.out.println("[" + NAME + "] v" + VERSION + " loaded!");
+    public void onInitialize() {
+        ModuleManager.init();
+        
+        openClickGui = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.oxygen.openclickgui",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_SHIFT,
+            "OxyGen Client"
+        ));
+        
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (openClickGui.wasPressed()) {
+                client.setScreen(ClickGUI.INSTANCE);
+            }
+        });
     }
 }
